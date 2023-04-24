b8f4fbbc7e0777d65652aa30aa66bf94fffc983a
facundoiba95 <facundoiba95@gmail.com>
2023-04-24 16:52:35 -0300

    add conditional enviroment

--- api/index.js
+++ api/index.js
@@ -13,8 +13,11 @@ const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json())
-app.use(express.urlencoded({ extended:false }));
+
+if(process.env.ENVIROMENT === 'production'){
+    app.use(express.json())
+    app.use(express.urlencoded({ extended:false }));
+} 
 
 app.use('/api/leagues', leaguesRoutes);
 app.use('/api/matches', matchesRoutes);
